386140ab3942163b52412c618c1b1774b017f33b
shivarao18 <shiva.thummanapalli@gmail.com>
2025-06-20 20:42:58 -0400

    lib.rs

--- programs/lending/src/lib.rs
+++ programs/lending/src/lib.rs
@@ -1,16 +1,43 @@
 use anchor_lang::prelude::*;
+use instructions::*;
 
-declare_id!("CUVw2rY1d7YSHL7WGXjhzwVnogbcr6i8zSmdwcdRmUYC");
+mod state;
+mod instructions;
+mod error;
+mod constants;
+
+declare_id!("CdZeD33fXsAHfZYS8jdxg4qHgXYJwBQ1Bv6GJyETtLST");
 
 #[program]
-pub mod lending {
+pub mod lending_protocol {
+
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+    pub fn init_bank(ctx: Context<InitBank>, liquidation_threshold: u64, max_ltv: u64) -> Result<()> {
+        process_init_bank(ctx, liquidation_threshold, max_ltv)
+    }
+
+    pub fn init_user(ctx: Context<InitUser>, usdc_address: Pubkey) -> Result<()> {
+        process_init_user(ctx, usdc_address)
+    }
+
+    pub fn deposit (ctx: Context<Deposit>, amount: u64) -> Result<()> {
+        process_deposit(ctx, amount)
+    }
+
+    pub fn withdraw (ctx: Context<Withdraw>, amount: u64) -> Result<()> {
+        process_withdraw(ctx, amount)
+    }
+
+    pub fn borrow(ctx: Context<Borrow>, amount: u64) -> Result<()> {
+        process_borrow(ctx, amount)
+    }
+
+    pub fn repay(ctx: Context<Repay>, amount: u64) -> Result<()> {
+        process_repay(ctx, amount)
+    }
+
+    pub fn liquidate(ctx: Context<Liquidate>) -> Result<()> {
+        process_liquidate(ctx)
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
